314b28080ff55be0544c31eefd4fc41485f29ffc
Terry Yu <terryzyu@gmail.com>
2018-05-20 11:33:33 -0500

    Added hw5 files

---
+++ hw5/Solver.java
@@ -0,0 +1,16 @@
+package hw5;
+
+public class Solver {
+	public static String solve(char[][] grid) {
+		// TODO
+		/*
+		 * 1. Construct a graph using grid
+		 * 2. Use BFS to find shortest path from start to finish
+		 * 3. Return the sequence of moves to get from start to finish
+		 */
+		
+		// Hardcoded solution to toyTest
+		return "RRRDDDDDDDDDRRRRRR";
+		
+	}
+}
